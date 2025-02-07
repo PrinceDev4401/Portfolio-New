57d7f5e55e074be7e02dc4107b1d7d9c8e21f3a3
Prince Dev <153496303+PrinceDev4401@users.noreply.github.com>
2025-02-07 01:21:55 +0000

    Create script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", function () {
+  var currentYear = new Date().getFullYear();
+  document.getElementById("currentYear").textContent = currentYear;
+});
+// showing the current year in the footer
